8702087bf91295e5d959716f482702a72b61b50d
Pattnatt <jittapat007@gmail.com>
2016-06-27 16:00:01 +0700

    Upload paddle's scripts

---
+++ Paddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour 
+{
+	float mouseX;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
+		mouseX = Input.mousePosition.x / Screen.width * 16;
+		paddlePos.x = Mathf.Clamp(mouseX, 0.5f , 15.5f);
+		
+		this.transform.position = paddlePos;
+	}
+}
