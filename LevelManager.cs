590d2fe3e02f2802820dc6b2cb75331abd1bc802
Pattnatt <jittapat007@gmail.com>
2016-06-29 21:33:22 +0700

    Update 29/06 #3

--- LevelManager.cs
+++ LevelManager.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour 
 {
+	public static int LastestLevel;
+
 	public void LoadLevel(string name)
 	{
+		LastestLevel = Application.loadedLevel;
 		Debug.Log ("Level load requested for: " + name);
 		Ball.started = false;
 		Application.LoadLevel(name);
@@ -12,9 +15,17 @@ public class LevelManager : MonoBehaviour
 	
 	public void LoadNextLevel()
 	{
+		LastestLevel = Application.loadedLevel;
 		Debug.Log ("Next level load requested for: " + Application.loadedLevel +1);
 		Ball.started = false;
 		Application.LoadLevel(Application.loadedLevel +1);
+	}
+	
+	public void LoadLastestLevel()
+	{
+		int temp = Application.loadedLevel;
+		Application.LoadLevel (LastestLevel);
+		LastestLevel = temp;
 	}
 	
 	public void QuitRequest()
